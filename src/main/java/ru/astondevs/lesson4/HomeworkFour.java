ce8a0a6da61dbd83834993d2858578f974b488af
Stas254 <Stas254@yandex.ru>
2024-02-28 10:37:51 +0300

    1. Выполнил ДЗ_4; 2. Ветка Lesson_4 втянут в Master; 3. Добавлен Класс "Person"; 4. Исправлено задание 2 (Employee переименовал в Person); 5. C&P.

--- src/main/java/ru/astondevs/lesson4/HomeworkFour.java
+++ src/main/java/ru/astondevs/lesson4/HomeworkFour.java
@@ -6,18 +6,17 @@ public class HomeworkFour {
         Employee employee = new Employee("Александрова Ирина", "Учитель", "alekstut@yandex.ru", "8-033-597-78-65", 1000.45, 52);
         employee.employeePrint();
         System.out.println("______---2---_______");
-        Employee[] empArray = new Employee[5];
-        empArray[0] = new Employee("Ivanov Ivan", "Engineer", "II@mail.ru", "892132587", 30000, 25);
-        empArray[1] = new Employee("Petrov Petr", "Engineer 2 сat", "PP@mail.ru", "8924587", 35000, 30);
-        empArray[2] = new Employee("Sidorov Serg", "Engineer 1 сat", "SS@mail.ru", "789872587", 40000, 35);
-        empArray[3] = new Employee("Lis Ivan", "Lead Engineer", "LI@mail.ru", "89546557", 45000, 40);
-        empArray[4] = new Employee("Luzan Ket", "Technologist", "LK@mail.ru", "86546545", 28000, 25);
-        for (int i = 0; i < empArray.length; i++)
-            System.out.println(empArray[i]);
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "II@mail.ru", "892132587", 30000, 25);
+        persArray[1] = new Person("Petrov Petr", "Engineer 2 сat", "PP@mail.ru", "8924587", 35000, 30);
+        persArray[2] = new Person("Sidorov Serg", "Engineer 1 сat", "SS@mail.ru", "789872587", 40000, 35);
+        persArray[3] = new Person("Lis Ivan", "Lead Engineer", "LI@mail.ru", "89546557", 45000, 40);
+        persArray[4] = new Person("Luzan Ket", "Technologist", "LK@mail.ru", "86546545", 28000, 25);
+        for (int i = 0; i < persArray.length; i++)
+            System.out.println(persArray[i]);
 
         // Смотри class MainPark для задания №3
         // System.out.println("______---3---_______");
-
 
     }
 }
